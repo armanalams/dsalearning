1fbd0cfeabeee41c7a64869f9a35b51732736d61
MD ARMAN ALAM <armanalamon@gmail.com>
2026-05-24 14:14:51 +0530

    Added Backtracking Array problem in Java

---
+++ backtrackArray.java
@@ -0,0 +1,34 @@
+public class backtrackArray {
+    public static void backtrackArray(int arr[],int i, int val)
+        {
+            //base case
+            if(i==arr.length)
+            {
+                printarr(arr);
+                return;
+            }
+
+            //recursion
+            arr[i]=val;
+            backtrackArray(arr, i+1, val+1);
+            arr[i]=arr[i]-2;
+
+        }
+        public static void printarr(int arr[])
+        {
+            for(int i=0; i<arr.length; i++)
+            {
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+        }
+
+
+    public static void main(String[] args) {
+
+        int arr[]=new int[5];
+        backtrackArray(arr, 0, 1 );
+        printarr(arr);
+        
+    }
+}
